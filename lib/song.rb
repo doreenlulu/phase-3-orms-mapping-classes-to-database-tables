e8ed3aa305c88ef686bcca50ffc7d35b8733986e
Doreen <doreenlulu97@gmail.com>
2022-11-08 01:02:43 +0300

    Solved

--- lib/song.rb
+++ lib/song.rb
@@ -27,12 +27,12 @@ class Song
       INSERT INTO songs (name, album)
       VALUES (?, ?)
     SQL
-    # insert the song
+    # inserts the song
     DB[:conn].execute(sql, self.name, self.album)
-    # get the song ID from the database and save it to the Ruby instance
+    # Save it to the Ruby instance
     self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
 
-    # return the Ruby instance
+    # return the specific instance
     self
 
   end
